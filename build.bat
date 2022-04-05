725536cc28b069949edade2c9e4ed6147b0309d6
Riccardo Gallina <rgall@sertiscorp.com>
2022-04-05 06:31:33 +0000

    Update CHANGELOG and version number

--- build.bat
+++ build.bat
@@ -59,7 +59,7 @@ IF NOT "%CLEAN%"=="" (
 )
 
 :: artifacts
-SET TAG=v0.1.0
+SET TAG=v0.2.0
 SET ARCHIVE_NAME=oneml-bootcamp-%TARGET_ARCH%.tar.gz
 SET BASE_URL=https://github.com/sertiscorp/oneML-bootcamp/releases/download/%TAG%/%ARCHIVE_NAME%
 IF NOT EXIST "%BINARY_PATH%\%ARCHIVE_NAME%" (
